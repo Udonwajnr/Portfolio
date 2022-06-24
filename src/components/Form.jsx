2662287d3a106ebc8db856a95dd43ce762536ea6
udonwajnr <udonwajnr10@gmail.com>
2022-06-24 16:26:09 +0100

    portfolio

---
+++ src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../components/assets/css/Form.css'
+const Form = () => {
+  return (
+    <div>
+        <div className="form-testing">
+            <input type="text" /> 
+            <span>
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Form
